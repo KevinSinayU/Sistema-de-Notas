02e428df92f2748bcb8e5c1a50a4886b02a0d400
KevinSinayU <ksinayl1@miumg.edu.gt>
2024-10-01 16:34:11 -0600

    Update run_app.sh

--- Sistema-de-Notas-main/run_app.sh
+++ Sistema-de-Notas-main/run_app.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
-#prompt for MySQL database credentials
-read -p "Enter MySQL database username: " DB_USER
-read -s -p "Enter MySQL database password: " DB_PASSWORD
+#solicitud de credenciales de la base de datos MySQL
+read -p "Ingrese el nombre de usuario de la base de datos MySQL: " DB_USER
+read -s -p "Ingrese la contraseña de la base de datos MySQL: " DB_PASSWORD
 echo
-read -p "Enter MySQL database namee: " DB_NAME
+read -p "Ingrese el nombre de la base de datos MySQL: " DB_NAME
 
-#Run MySQL setup
-echo "Setting up  MySQL database schema..."
-mysql -u $DB_USER -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXIST $DB_NAME;"
+#Ejecute la configuración de MySQL
+echo "Configurando el esquema de la base de datos MySQL..."
+mysql -u $DB_USER -p$DB_PASSWORD -e "CREAR BASE DE DATOS SI NO EXISTE $DB_NAME;"
 
-#Prompt for paths to Spring Boot and React apps
-read -p "Enter path to Spring Boot app: " SPRING_BOOT_PATH
-read -p "Enter path to React app: " REACT_PATH
+#Solicitar rutas a las aplicaciones Spring Boot y React
+read -p "Ingresar ruta a la aplicación Spring Boot: " SPRING_BOOT_PATH
+read -p "Ingresar ruta a la aplicación React: " REACT_PATH
 
 APPLICATION_PROPERTIES_PATH="$SPRING_BOOT_PATH/src/main/resources/application.properties"
 
-#Configure Spring Boot application properties 
-echo "Configuring Spring Boot application properties..."
-cp "$APPLICATION_PROPERTIES_PATH" "$APPLICATION_PROPERTIES_PATH.bak" #backup original file
+#Configurar propiedades de la aplicación Spring Boot
+echo "Configurando propiedades de la aplicación Spring Boot..."
+cp "$APPLICATION_PROPERTIES_PATH" "$APPLICATION_PROPERTIES_PATH.bak" #Realizar copia de seguridad del archivo original
 sed -i "s/spring.datasource.username=your_username/spring.datasource.username=$DB_USER/" "$APPLICATION_PROPERTIES_PATH"
-sed -i "s/spring.datasource.password=your_password/spring.datasource.username=$DB_PASSWORD/" "$APPLICATION_PROPERTIES_PATH"
-sed -i "s/spring.datasource.url=jdbc:mysql\/\/localhost:3306\/your_db/spring.datasource.url=jdbc:mysql\/\/localhost:3306\/$DB_NAME/" "$APPLICATION_PROPERTIES_PATH"
+sed -i "s/spring.datasource.password=su_contraseña/spring.datasource.username=$DB_PASSWORD/" "$APPLICATION_PROPERTIES_PATH"
+sed -i "s/spring.datasource.url=jdbc:mysql\/\/localhost:3306\/su_base_de_datos/spring.datasource.url=jdbc:mysql\/\/localhost:3306\/$DB_NAME/" "$APPLICATION_PROPERTIES_PATH"
 
-#Run Sprin Boot with DB migration
-echo "Starting Spring Boot..."
+#Ejecutar Spring Boot con migración de base de datos
+echo "Iniciando Spring Boot..."
 cd "$SPRING_BOOT_PATH"
 ./mvnw clean install
 ./mvnw spring-boot:run &
 
-
-#Wait for backend to start
+#Esperar a que se inicie el backend
 sleep 10
 
-#Run React
-echo "Starting React..."
+#Ejecutar React
+echo "Iniciando React..."
 cd "$REACT_PATH"
 npm install
 npm start
 
-#Exit the apps
-echo "Stopping Spring Boot..."
+#Salir las aplicaciones
+echo "Deteniendo Spring Boot..."
 pkill -f "spring-boot"
 
-echo "Stopping React..."
+echo "Deteniendo React..."
 pkill -f "react-scripts"
 
-echo "Apps stopped successfully"
+echo "Las aplicaciones se detuvieron correctamente"
